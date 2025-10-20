7d9d3b57b2a06e2f56182e531366f3cc24a52d66
Lucas França <lucasfilipefn13@gmail.com>
2025-10-20 16:42:55 -0300

    Update MainApp.java
    ....

--- src/main/java/org/example/labirintothreads/MainApp.java
+++ src/main/java/org/example/labirintothreads/MainApp.java
@@ -10,13 +10,13 @@ import java.io.IOException;
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        // FXMLLoader carrega a interface a partir do arquivo FXML.
+        // FXMLLoader carrega a interface a partir do arquivo FXML
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("labirinto-view.fxml"));
-        // Cria a "Cena" com o layout carregado do FXML, definindo a largura e altura.
+        // Cria a "Cena" com o layout carregado do FXML, definindo a largura e altura
         Scene scene = new Scene(fxmlLoader.load(), 650, 600);
-        // O "Stage" é a janela principal da aplicação.
+        // O "Stage" e a janela principal da aplicacao
         stage.setTitle("Labirinto com Threads");
-        stage.setScene(scene); // Coloca a cena na janela.
+        stage.setScene(scene); // Coloca a cena na janela
         stage.show(); //Exibe a janela
     }
 
